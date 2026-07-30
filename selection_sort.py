55a0db88c90129282353d226ec745d2726a89d09
Hriday Das <dashriday856@gmail.com>
2026-07-30 17:24:03 +0530

    Create selection_sort.py

---
+++ selection_sort.py
@@ -0,0 +1,7 @@
+nums = [20,1,5,0,20,9]
+for i in range(len(nums)):
+    minimum = i
+    for j in range(i + 1, len(nums)):
+        if nums[j] > nums[i]:
+            nums[j], nums[minimum] = nums[minimum], nums[j]
+print(nums)
