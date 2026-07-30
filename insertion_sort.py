f9768ce0e624bc892dc5f333c451c8d7d9871452
Hriday Das <dashriday856@gmail.com>
2026-07-30 17:23:25 +0530

    Create insertion_sort.py

---
+++ insertion_sort.py
@@ -0,0 +1,9 @@
+nums = [2,3,1,6,5,4]
+for i in range(1, len(nums)):
+    key = nums[i]
+    j = i - 1
+    while j >= 0 and key < nums[j]:
+        nums[j+1] = nums[j]
+        j -= 1
+    nums[j + 1] = key
+print(nums)
