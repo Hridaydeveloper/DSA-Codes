04b48de6a673c8ae0bc3d2aadd4b38eeb4273a39
Hriday Das <dashriday856@gmail.com>
2026-08-21 02:26:51 +0530

    Fix selection sort comparison operator

--- selection_sort.py
+++ selection_sort.py
@@ -2,6 +2,6 @@ nums = [20,1,5,0,20,9]
 for i in range(len(nums)):
     minimum = i
     for j in range(i + 1, len(nums)):
-        if nums[j] > nums[i]:
+        if nums[j] < nums[minimum]:
             nums[j], nums[minimum] = nums[minimum], nums[j]
 print(nums)
